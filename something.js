5869ed4ecd59111c1631dd2f93beba31096aa074
Matthew Marshall <2879+mmarshall@users.noreply.github.com>
2008-10-12 13:59:28 -0500

    Added join and part events.

--- something.js
+++ something.js
@@ -1,10 +1,17 @@
 events.getPlayerSpawnPos.add(function () {
-    this.player.sendMessage("Air Spawn!" + this.player.currentFlag);
-    this.pos[2] += Math.random()*10;
+    if (this.player.spawn_height)
+        this.pos[2] += this.player.spawn_height;
 });
 
-slash_commands.doit = function(word) {
-    this.from.sendMessage(word);
-    this.from.sendMessage(this.message);
+slash_commands.spawnheight = function(height) {
+    if (isNaN(height)) {
+        this.from.sendMessage("'"+height+"' isn't a number!");
+        return;
+    }
+    this.from.spawn_height = Number(height);
+    this.from.sendMessage("Set your spawn height to "+height);
 }
 
+events.playerJoin.add(function (){
+    this.player.sendMessage("hey there!");
+});
