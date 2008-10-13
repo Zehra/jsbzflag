e2c364129776ee98fe01272bafd61e626a7f8c3e
mmarshall <mmarshall@puma>
2008-10-13 13:47:05 -0500

    Added Team objects.

--- something.js
+++ something.js
@@ -1,17 +1,7 @@
+// This is just a scratchpad as I test and debug new stuff.
 
-events.getPlayerSpawnPos.add(function () {
-    if (this.player.last_die_pos)
-        this.pos = this.player.last_die_pos;
-    this.player.sendMessage("spawning");
+events.playerJoin.add(function() {
+    this.player.team.sendMessage(this.player.team.name);
 });
 
-events.playerDie.add(function() {
-    this.player.last_die_pos = this.pos;
-});
-
-slash_commands.gimme = function(flag) {
-    print(flag);
-    //this.from.removeFlag();
-    print(this.from.giveFlag(flag, true));
-}
 
